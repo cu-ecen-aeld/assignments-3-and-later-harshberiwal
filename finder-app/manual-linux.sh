25ff17d2dd0c7a497ccd55bfff8a4f1a1a142d3b
harshberiwal <harshberiwal@Ubuntu20.harsh.virtualbox.org>
2023-02-05 10:15:51 -0700

    Part 2 Run Successfully

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -33,16 +33,17 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
+    
+    #Add your kernel build steps here
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
-    echo "Hello 2"
-    # TODO: Add your kernel build steps here
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -54,8 +55,7 @@ fi
 
 
 
-# TODO: Create necessary base directories
-
+# Create necessary base directories
 mkdir -p "${OUTDIR}/rootfs"
 cd "${OUTDIR}/rootfs"
 mkdir bin dev etc home lib lib64 proc sbin sys tmp usr var 
@@ -68,8 +68,8 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-   
+    
+    #Configure busybox
     make distclean 
     make defconfig
 
@@ -77,7 +77,7 @@ else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
 make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
@@ -87,7 +87,7 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
 cd "${OUTDIR}/rootfs"
 export SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
 
@@ -101,17 +101,17 @@ cp -a $SYSROOT/lib64/libresolv.so.2 lib64
 cp -a $SYSROOT/lib64/libresolv-2.31.so lib64
 
 echo "Coming" 
-# TODO: Make device nodes
+# Make device nodes
 cd "${OUTDIR}/rootfs"
 sudo mknod -m 666 dev/null c 1 3 
 sudo mknod -m 600 dev/concole c 5 1 
 
-# TODO: Clean and build the writer utility
+# Clean and build the writer utility
 cd ${FINDER_APP_DIR}
 make clean 
 make CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 if [ -f writer ]
 then
@@ -123,11 +123,11 @@ then
     cp autorun-qemu.sh "$OUTDIR/rootfs/home/"
 fi
 
-# TODO: Chown the root directory
+# Chown the root directory
 cd "${OUTDIR}/rootfs"
 sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
 cd "${OUTDIR}/rootfs"
 find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
 cd ..
