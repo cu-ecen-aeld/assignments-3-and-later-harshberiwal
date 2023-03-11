669f8498e23ff14949cec2c6418a83859195efb2
harshberiwal <harshberiwal@Ubuntu20.harsh.virtualbox.org>
2023-03-10 23:01:55 -0700

    Circular Buffer Implemented

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -16,6 +16,12 @@
 
 #include "aesd-circular-buffer.h"
 
+
+uint8_t nextPtr(uint8_t ptr){
+    ptr = (ptr+1)%(AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED);
+    return ptr; 
+}
+
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
@@ -29,10 +35,21 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
+    struct aesd_buffer_entry *found_entry; 
+	uint8_t read_ptr =buffer -> out_offs; 
+	uint8_t count =0; 
+	while(count++ < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+		if(char_offset >= (buffer -> entry[read_ptr].size)) {
+			char_offset = char_offset - (buffer -> entry[read_ptr].size); 
+		}
+		else {
+			*entry_offset_byte_rtn = char_offset;
+			found_entry = &(buffer -> entry[read_ptr]); 
+			return found_entry;
+		}
+		read_ptr = nextPtr(read_ptr);
+	}
+	return NULL;  
 }
 
 /**
@@ -44,6 +61,17 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
+    buffer -> entry[buffer-> in_offs] = *add_entry;  
+    buffer -> in_offs = nextPtr(buffer -> in_offs);	
+    if(buffer->full == false){
+         if(buffer -> in_offs == buffer -> out_offs) {
+                buffer -> full = true; 
+         }
+    }   
+    else {
+         //buffer -> in_offs = nextPtr(buffer -> in_offs);	
+         buffer -> out_offs = nextPtr(buffer -> out_offs);	 
+    }
     /**
     * TODO: implement per description
     */
@@ -56,3 +84,5 @@ void aesd_circular_buffer_init(struct aesd_circular_buffer *buffer)
 {
     memset(buffer,0,sizeof(struct aesd_circular_buffer));
 }
+
+
