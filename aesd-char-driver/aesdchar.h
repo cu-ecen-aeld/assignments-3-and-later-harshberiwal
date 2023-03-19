830380273351c11105ff1744ffa2fb6510676738
harshberiwal <111919475+harshberiwal@users.noreply.github.com>
2023-03-19 04:48:19 -0600

    Update aesdchar.h

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -31,7 +31,7 @@ struct aesd_dev
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
     struct aesd_circular_buffer circularBuffer;
-    struct aesd_buffer_entry element;	
+    struct aesd_buffer_entry entry;	
     struct mutex lock; 
     struct cdev cdev;     /* Char device structure      */
 };
